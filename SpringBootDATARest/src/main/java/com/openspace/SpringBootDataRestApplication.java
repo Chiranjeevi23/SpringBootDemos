127dfeaa68927d74211f2c735ce31f8e4ccdc0dd
ChiranjeeviN <ChiranjeeviN@DESKTOP-0C8J1P3>
2018-07-13 15:32:22 +0530

    Spring Data REST

---
+++ SpringBootDATARest/src/main/java/com/openspace/SpringBootDataRestApplication.java
@@ -0,0 +1,12 @@
+package com.openspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataRestApplication.class, args);
+	}
+}
---
+++ SpringBootDATARest/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into alien values 
+(104, 'Zeus', 'Greek'),
+(106, 'Poseidon', 'Greek'),
+(103, 'Apollo', 'nonGreek'),
+(102, 'Hades', 'nonGreek'),
+(101, 'Kratos', 'Spartan'),
+(105, 'Helios', 'Greek');
+
